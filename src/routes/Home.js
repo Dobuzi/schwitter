c0e3a215faefaf816897f343d5fc688b4418f4c9
Dobuzi <kimjw7981@gmail.com>
2020-09-16 21:38:19 +0900

    schweet set up

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [schweet, setSchweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setSchweet(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={schweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type="submit" value="Schweet" />
+            </form>
+        </div>
+    );
+};
 
 export default Home;
