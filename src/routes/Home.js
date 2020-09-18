860eb60e9a8643902eb1a38af519a5d5c378925f
Dobuzi <kimjw7981@gmail.com>
2020-09-18 12:54:18 +0900

    image upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import Pang from "../components/Pang";
 const Home = ({ userObj }) => {
     const [pang, setPang] = useState("");
     const [pangs, setPangs] = useState([]);
+    const [attachment, setAttachment] = useState(null);
     useEffect(() => {
         dbService.collection("pangs").onSnapshot((snapshot) => {
             const pangArray = snapshot.docs.map((doc) => ({
@@ -30,6 +31,21 @@ const Home = ({ userObj }) => {
         } = event;
         setPang(value);
     };
+    const onFileChange = (event) => {
+        const {
+            target: { files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                target: { result },
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearClick = () => setAttachment(null);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -40,7 +56,19 @@ const Home = ({ userObj }) => {
                     placeholder="What's on your mind?"
                     maxLength={120}
                 />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Pang" />
+                {attachment && (
+                    <div>
+                        <img
+                            src={attachment}
+                            width="50px"
+                            height="50px"
+                            alt="profile"
+                        />
+                        <button onClick={onClearClick}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {pangs.map((pang) => (
