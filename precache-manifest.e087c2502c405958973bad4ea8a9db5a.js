96785bfd79663c6006efb0c79f67e1516ca991c2
Dobuzi <kimjw7981@gmail.com>
2020-09-21 12:08:10 +0900

    Updates

---
+++ precache-manifest.e087c2502c405958973bad4ea8a9db5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb12d9b1dacaf3b52cd81dce2b4346ba",
+    "url": "/schwitter/index.html"
+  },
+  {
+    "revision": "9b4fb1049ac5843b004b",
+    "url": "/schwitter/static/css/main.7102eae1.chunk.css"
+  },
+  {
+    "revision": "b6c63e280926fc545e8a",
+    "url": "/schwitter/static/js/2.93b8df73.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/schwitter/static/js/2.93b8df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4fb1049ac5843b004b",
+    "url": "/schwitter/static/js/main.c4cbf7b6.chunk.js"
+  },
+  {
+    "revision": "4cade3244af9850deacb",
+    "url": "/schwitter/static/js/runtime-main.f38b8717.js"
+  }
+]);
