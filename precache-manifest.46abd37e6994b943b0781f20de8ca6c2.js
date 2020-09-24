80dcc6d5accd2ab6c9042baaf0c6ea2cd63a8967
Dobuzi <kimjw7981@gmail.com>
2020-09-24 15:10:00 +0900

    Updates

---
+++ precache-manifest.46abd37e6994b943b0781f20de8ca6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5166c789082c8a01f042fa81a6b0b6",
+    "url": "/schwitter/index.html"
+  },
+  {
+    "revision": "6ca57219f4b455f2d4f0",
+    "url": "/schwitter/static/css/main.b69f5ba2.chunk.css"
+  },
+  {
+    "revision": "b4e2b470cbe350e58544",
+    "url": "/schwitter/static/js/2.f6980141.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/schwitter/static/js/2.f6980141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca57219f4b455f2d4f0",
+    "url": "/schwitter/static/js/main.4cb2a6c9.chunk.js"
+  },
+  {
+    "revision": "4cade3244af9850deacb",
+    "url": "/schwitter/static/js/runtime-main.f38b8717.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/schwitter/precache-manifest.8de626d0a4c56e281d0cc7b6bc47109a.js"
+  "/schwitter/precache-manifest.46abd37e6994b943b0781f20de8ca6c2.js"
 );
 
 self.addEventListener('message', (event) => {
